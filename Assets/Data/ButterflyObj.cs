ffc9a85dd4febf90bb968361b50bc1091b0e9485
laic00 <dich0000@mail.ru>
2020-03-31 17:44:55 +0500

    MVC ScriptableObject

---
+++ Assets/Data/ButterflyObj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ButterflyF
+{
+    [CreateAssetMenu(fileName= "RotationSpeed", menuName= "CreateScriptableObject/RotationSpeed", order=1)]
+    public sealed class RotationSpeed : ScriptableObject
+    {
+        // Start is called before the first frame update
+
+        [Range(0.0f, 1.0f)]
+        public float Speed;
+
+      
+
+    }
+}
---
+++ Assets/mvc/ButterflyController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ButterflyF
+{
+    public class ButterflyController : MonoBehaviour
+    {
+        private Animator _anim;
+        private SpriteRenderer _sr;
+        private Rigidbody2D _rb;
+        public Vector2 direction;
+        [SerializeField] private RotationSpeed _speed;
+        [SerializeField] private RotationRightBorder _rightBorder;
+      
+
+    
+       
+
+        // Start is called before the first frame update
+
+        void Start()
+        {
+
+           
+            _rb = GetComponent<Rigidbody2D>();
+            _sr = GetComponent<SpriteRenderer>();
+        }
+
+        void Stop()
+        {
+            _anim.SetBool("bfiy_stop", false);
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
+
+        }
+
+        void FixedUpdate()
+        {
+
+            transform.Translate(direction.normalized * _speed.Speed);
+            if (transform.position.x > _rightBorder.RightBorder)
+            {
+                _sr.flipX = true;
+                Stop();
+            }
+
+          
+
+        }
+    }
+}
